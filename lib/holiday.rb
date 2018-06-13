52a95146071b0d9957a8633d55e572a183157ea6
Jeremy Parker <jrobinparker@gmail.com>
2018-06-13 14:20:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,8 +42,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
      }
    }
    
-   holiday_hash.each do |season|
-     
+   holiday_hash.each do |season, holiday|
+      if season == :winter
+        holiday_hash[:winter] = [:]
+      end
+    end 
    
    
 end
